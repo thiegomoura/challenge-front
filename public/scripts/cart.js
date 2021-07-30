220b386a81e19fdb87d96dc9aeb27ad5b3b915b3
thiegomoura <thi-ego@hotmail.com>
2021-07-30 13:08:46 -0400

    add mobile menu

--- public/scripts/cart.js
+++ public/scripts/cart.js
@@ -1,10 +1,17 @@
 const cart = document.getElementById("cart");
-const cartIcon = document.getElementById("cart-icon")
-let isVisible = true;
-
+const cartIcon = document.getElementById("cart-icon");
+const menuToggle = document.getElementById("menu-toggle");
+const iconMenuToggle = document.getElementById("icon-menu-toggle");
+let carVisible = true;
+let menuVisible = true;
 
 cartIcon.addEventListener("click", () => {
     cartIcon?.classList.toggle("select");
-    cart?.classList.toggle("open", isVisible)
-    isVisible = !isVisible;
+    cart?.classList.toggle("open", carVisible)
+    carVisible = !carVisible
+})
+
+iconMenuToggle.addEventListener("click", () => {
+    menuToggle.classList.toggle("open", menuVisible)
+    menuVisible = !menuVisible
 })
