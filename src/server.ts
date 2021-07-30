e0db641fd88061158f900fc07541ce55f0c8ee81
Thiego Moura <thi-ego@hotmail.com>
2021-07-29 22:18:12 -0400

    add config view on the server

--- src/server.ts
+++ src/server.ts
@@ -7,12 +7,14 @@ dotenv.config()
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.set('views', __dirname + '/views')
+app.engine('html', require('ejs').renderFile)
 
-app.get('/', (req, res) => {
-    res.send({message: 'Hello agenciaPlus'})
+app.get('/', async (req, res) => {
+    res.render('index.html');
 })
 
 const port = process.env.SERVER_PORT
 app.listen(port || 3333, () => {
-    console.log(`🏃‍♂️ server is running on port ${port || 3333}`)
+    console.log(`👍 server is running on port ${port || 3333}`)
 })
